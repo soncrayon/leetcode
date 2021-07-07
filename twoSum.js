e348566f60f7754a147a2941a76128031e4980e7
soncrayon <arnold.curtis@gmail.com>
2021-07-06 22:49:09 -0400

    twosum brute solution

--- twoSum.js
+++ twoSum.js
@@ -1,17 +1,10 @@
 const twoSum = (nums, target) => {
-    nums.sort()
     for (let i=0; i<nums.length; i++){
-        let indexArr = []
-        indexArr.push(nums[i])
-        let neededNum = target - nums[i]
         for (let j=0; j<nums.length; j++){
-            if (i !== j){
-                if (nums[j] + neededNum > target){
-                break 
-                } else if (neededNum === nums[j]){
-                    indexArr.push(nums[j])
-                    return indexArr
-                }
+            if (i === j){ 
+                continue 
+            } else if (nums[i] + nums[j] === target){
+                return [i,j]
             }
         }
     }
