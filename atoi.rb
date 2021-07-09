e1b6470cd434a5be5a6939d93b6556dace0d4344
soncrayon <arnold.curtis@gmail.com>
2021-07-08 23:35:12 -0400

    ruby atoi partial solution

---
+++ atoi.rb
@@ -0,0 +1,30 @@
+def atoi (s)
+    s_to_change = ''
+    negator = 1
+    s_arr = s.split('')
+    s_arr.each_with_index do |i, idx|
+        if (s_arr[idx] === "+" || s_arr[idx] === "-") &&
+            ((s_arr[idx - 1] === "+" || s_arr[idx - 1] === "-") || s_to_change.length > 0) &&
+            idx != 0
+            break 
+        elsif i === ' ' && s_to_change.length > 0
+            break
+        elsif i === ' '||i === "+"
+            next 
+        elsif i === '-'
+            negator = -1
+        elsif i.to_i.class == Integer 
+            s_to_change += i
+        else
+            break 
+        end
+    end
+    s_to_change = s_to_change.to_i * negator
+    if s_to_change < -2**31 
+        return -2**31
+    elsif s_to_change > (2**31 - 1)
+        return 2**31 - 1
+    else
+        return s_to_change
+    end
+end
