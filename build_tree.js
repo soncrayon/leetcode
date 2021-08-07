c38c11ad33adbbe119128637570f7a0e8ba9b1a6
soncrayon <arnold.curtis@gmail.com>
2021-08-07 11:39:21 -0400

    build tree testing

---
+++ build_tree.js
@@ -0,0 +1,28 @@
+class Tree {
+    initialize(){
+        Object.new()
+    }
+
+    addNewNodeToTree = (treeNode) => {
+        Object.keys(treeNode).forEach((key) => {
+            if (this[key]) {
+                // this[key] = treeNode[key]
+                this.addNewNodeToTree(treeNode[key])
+            }
+            // set up recursive function to check each level of tree for value and if not add to appropriate level
+        })
+    }
+}
+
+let newTree = new Tree
+
+console.log(newTree)
+
+newTree.addNewNodeToTree({'newNode': [{'just':['testing'], 'how':['works?']}]})
+newTree.addNewNodeToTree({'secondNode': [{'just2':['testing2'], 'how2':['works?2']}]})
+newTree.addNewNodeToTree({'just2':['colors']})
+
+
+
+
+console.log(newTree)
