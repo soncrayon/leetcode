51da161b987e8ad8e78d34ef8dfdcc78e02bab27
soncrayon <arnold.curtis@gmail.com>
2021-08-08 10:57:17 -0400

    linked list buildout

---
+++ 01. linked_list_buildout.js
@@ -0,0 +1,70 @@
+//create head for linked list and Node class
+
+class LinkedList {
+  constructor(h) {
+    this.head = h;
+  }
+
+  addNodeToFront = (d) => {
+    let newNode = new Node(d);
+    newNode.next = this.head;
+    this.head = newNode;
+  };
+
+  addNodeAfterAnotherNode = (previousNode, d) => {
+    if (previousNode == null) {
+      return;
+    }
+
+    let newNode = new Node(d);
+    newNode.next = previousNode.next;
+    previousNode.next = newNode;
+  };
+
+  addNodeToBack = (d) => {
+    const traverseList = (node) => {
+      if (node.next != null) {
+        node = node.next;
+        return traverseList(node);
+      } else {
+        return node;
+      }
+    };
+    let newNode = new Node(d);
+    let lastNode = traverseList(this.head);
+    lastNode.next = newNode;
+  };
+
+  findNode = (d) => {
+      const traverseList = (node) => {
+          if (node.data != d) {
+              node = node.next
+              return traverseList(node)
+          } else {
+              return node
+          }
+      }
+      return traverseList(this.head)
+  }
+}
+
+class Node {
+  constructor(d) {
+    this.data = d;
+    this.next = null;
+  }
+}
+
+let head = new Node ('a')
+let linkedList = new LinkedList(head)
+linkedList.addNodeToBack('c')
+linkedList.addNodeToBack('e')
+linkedList.addNodeToBack('g')
+linkedList.addNodeAfterAnotherNode(linkedList.findNode('a'), 'b')
+linkedList.addNodeAfterAnotherNode(linkedList.findNode('c'), 'd')
+linkedList.addNodeAfterAnotherNode(linkedList.findNode('e'), 'f')
+
+
+console.log(linkedList.findNode('f'))
+
+
