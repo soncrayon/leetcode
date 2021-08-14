da26326b2d3be26d8dd859b8695938c2270bd314
soncrayon <arnold.curtis@gmail.com>
2021-08-14 00:07:27 -0400

    reverse integer light refactor

--- reverseInteger.js
+++ reverseInteger.js
@@ -1,17 +1,20 @@
 const reverseInteger = (x) => {
-    let numToRev = x
-    if ( x < 0 ) numToRev = -numToRev
-    numToRev = `${x}`
+    if (`${x}`.length > 10) return 0 
+    let numToRev = `${x}`
     let numArr = numToRev.split('')
-    let i = 0, j = numArr.length - 1, temp
-    while ( i < j) {
+    if ( x < 0 ) numArr.shift()
+    let i = 0, temp
+    while ( i < (numArr.length - (i + 1))) {
         temp = numArr[i]
-        numArr[i] = numArr[j]
-        numArr[j] = temp 
+        numArr[i] = numArr[numArr.length - (i + 1)]
+        numArr[numArr.length - (i + 1)] = temp 
         i++
-        j--
     }
     numToRev = parseInt(numArr.join(''))
-    if ( x < 0 ) numToRev = -numToRev 
+    if ( x < 0 ) numToRev = -numToRev
     return (numToRev > 2**31) || (numToRev < -(2**31)) ? 0 : numToRev 
 };
+
+console.log(reverseInteger(12357))
+console.log(reverseInteger(-20356))
+console.log(reverseInteger(976543219876543))
