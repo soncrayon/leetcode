bb79ba4b208ce794c3fc5120e06a28eff204f886
soncrayon <arnold.curtis@gmail.com>
2021-09-18 23:18:17 -0400

    shifted binary search2

--- shifted_binary_search.js
+++ shifted_binary_search.js
@@ -1,16 +1,11 @@
 const shiftedBinarySearch = (array, target) => {
-
-      let newArray = [...array]; 
-      
+      let newArray = [...array];
       newArray.sort((a, b) => a - b)
-      
-      console.log(binarySearch(newArray, target))
       return array.indexOf(binarySearch(newArray, target));
   }
   
   const binarySearch = (arr, target) => {
       let start = 0, end = arr.length - 1;
-      
       while ( start <= end ){
           let mid = Math.floor( (start + end) / 2);
           console.log(arr[mid])
@@ -21,7 +16,5 @@ const shiftedBinarySearch = (array, target) => {
               start = mid + 1; 
           }
       }
-       
       return -1;
-      
   }
